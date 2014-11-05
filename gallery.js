8d578e383b49e7689775f7da00c16525d6aca6ea
Raffaella Migliaccio <raffaella.migliacico92@gmail.com>
2014-11-05 18:21:22 -0500

    Aggiunta gallery

---
+++ gallery.js
@@ -0,0 +1,242 @@
+var gallery = [
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_5596.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_5596.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_5606.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_5606.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_5624.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_5624.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_5637.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_5637.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_5650.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_5650.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_5651.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_5651.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_5660.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_5660.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_5671.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_5671.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_5691.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_5691.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_5695.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_5695.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_5721.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_5721.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_5725.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_5725.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_5728.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_5728.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_5742.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_5742.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_5745.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_5745.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_5758.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_5758.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_5764.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_5764.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_5784.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_5784.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_5794.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_5794.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_5976.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_5976.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_5978.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_5978.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_5994.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_5994.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_6019.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_6019.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_6033.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_6033.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_6052.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_6052.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_6055.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_6055.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_6077.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_6077.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_6101.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_6101.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_6105.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_6105.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_6117.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_6117.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_6124.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_6124.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_6225.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_6225.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_6235.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_6235.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_6244.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_6244.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_6245.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_6245.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_6269.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_6269.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_6274.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_6274.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_6277.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_6277.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_6278.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_6278.JPG'
+    },
+    {
+        title: '',
+        href: 'assets/img/gallery/DSC_6279.JPG',
+        type: 'image/jpeg',
+        thumbnail: 'assets/img/gallery/t/DSC_6279.JPG'
+    },
+];
